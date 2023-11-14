15f745b67f56c4572c2bf39c84fedeff9cc1a509
Axxzjavon Sampson <axxzjano@gmail.com>
2023-11-13 19:09:02 -0600

    Completed functions exercise

--- js/functions.js
+++ js/functions.js
@@ -1,49 +1,104 @@
 "use strict";
 
-/**
- * Function to say hello to the passed-in name.
- * @param {string} name - The name to greet.
- * @returns {string} - The greeting message.
- */
+// /**
+//  * TODO:
+//  * Create a function called 'sayHello' that takes a parameter 'name'.
+//  * When called, the function should return a message that says hello to the passed-in name.
+//  *
+//  * Example
+//  * > sayHello("codeup") // returns "Hello, codeup!"
+//  */
 function sayHello(name) {
-    alert('Hello ' + name + '!');
+    return "Hello, " + name + "!";
 }
 
-// Call the function with a string literal argument
+// /**
+//  * TODO:
+//  * Call the function 'sayHello' and pass your name as a string literal argument.
+//  * Store the result of the function call in a variable named 'helloMessage'.
+//  *
+//  * console.log 'helloMessage' to check your work
+//  */
 let helloMessage = sayHello("YourName");
 console.log(helloMessage);
 
-// Store your name in a variable and pass it to the function
+// /**
+//  * TODO:
+//  * Store your name as a string in a variable named 'myName', and pass that
+//  * variable to the 'sayHello' function. You should see the same output in the
+//  * console.
+//  */
 let myName = "YourName";
-helloMessage = sayHello(myName);
-console.log(helloMessage);
+console.log(sayHello(myName));
 
-// Function to check if a number is equal to 2
+// Don't modify the following line, it generates a random number between 1 and 3
+// and stores it in a variable named random
+const random = Math.floor((Math.random() * 3) + 1);
+
+// /**
+//  * TODO:
+//  * Create a function called 'isTwo' that takes a number as a parameter.
+//  * The function should return a boolean value based on whether or not the passed
+//  * number is the number 2.
+//  *
+//  * Example
+//  * > isTwo(1) // returns false
+//  * > isTwo(2) // returns true
+//  * > isTwo(3) // returns false
+//  *
+//  * Call the function 'isTwo' passing the variable 'random' as an argument.
+//  *
+//  * console.log *outside of the function* to check your work (you should see a
+//  * different result every time you refresh the page if you are using the random
+//  * number)
+//  */
 function isTwo(number) {
     return number === 2;
 }
 
-// Call the function 'isTwo' passing the variable 'random' as an argument
 console.log(isTwo(random));
 
-// Function to calculate tip based on tip percentage and total bill
-function calculateTip(tipPercentage, totalBill) {
-    return tipPercentage * totalBill;
+// /**
+//  * TODO:
+//  * Create a function named 'calculateTip' to calculate a tip on a bill at a
+//  * restaurant. The function should accept a tip percentage and the total of the
+//  * bill and return the amount to tip
+//  *
+//  * Examples:
+//  * > calculateTip(0.20, 20) // returns 4
+//  * > calculateTip(0.25, 25.50) // returns 6.375
+//  * > calculateTip(0.15, 33.42) // returns 5.013
+//  */
+function calculateTip(tipPercentage, billTotal) {
+    return tipPercentage * billTotal;
 }
 
-// Prompt user for bill total and tip percentage, then display the tip amount
-let billTotal = parseFloat(prompt("Enter the bill total:"));
-let tipPercentage = parseFloat(prompt("Enter the tip percentage:")) / 100;
-let tipAmount = calculateTip(tipPercentage, billTotal);
+// /**
+//  * TODO:
+//  * Use prompt and alert in combination with your calculateTip function to
+//  * prompt the user for the bill total and a percentage they would like to tip,
+//  * then display the dollar amount they should tip
+//  */
+let userBillTotal = parseFloat(prompt("Enter the bill total:"));
+let userTipPercentage = parseFloat(prompt("Enter the tip percentage (as a decimal):"));
+
+let tipAmount = calculateTip(userTipPercentage, userBillTotal);
 alert("You should tip: $" + tipAmount.toFixed(2));
 
-// Function to apply a discount to the original price
+// /**
+//  * TODO:
+//  * Create a function named `applyDiscount`. This function should accept a price
+//  * (before a discount is applied), and a discount percentage (a number between 0
+//  * and 1). It should return the result of applying the discount to the original
+//  * price.
+//  *
+//  * Example:
+//  * > let originalPrice = 100;
+//  * > let discountPercent = .2; // 20%
+//  * > applyDiscount(originalPrice, discountPercent) // 80
+//  *
+//  * > applyDiscount(45.99, 0.12) // 40.4712
+//  */
 function applyDiscount(originalPrice, discountPercent) {
-    return originalPrice * (1 - discountPercent);
+    return originalPrice - (originalPrice * discountPercent);
 }
-
-// Example usage of applyDiscount function
-let originalPrice = 100;
-let discountPercent = 0.2; // 20%
-let discountedPrice = applyDiscount(originalPrice, discountPercent);
-console.log(discountedPrice);
