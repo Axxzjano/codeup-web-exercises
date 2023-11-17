0d3fd8309b94e8e246f8df6abe9f6c02c50aa078
Axxzjavon Sampson <axxzjano@gmail.com>
2023-11-17 10:01:45 -0600

    fixed while loop

--- js/practice.js
+++ js/practice.js
@@ -1,3 +1,4 @@
+'use strict'
 // function calculateTotal(luckyNumber, totalAmount) {
 //     switch (luckyNumber) {
 //         case 0:
@@ -26,32 +27,97 @@
 // // Slytherin
 // // Gryffindor
 //
-let pickNumber = prompt("Pick a number 1-4")
-let chooseColor = prompt('Choose a color')
-let chooseObject = prompt('Choose an object')
-let chooseCreature = prompt("Choose a creature")
-let chooseHobby = prompt("Choose a hobby")
+// let pickNumber = prompt("Pick a number 1-4")
+// let chooseColor = prompt('Choose a color')
+// let chooseObject = prompt('Choose an object')
+// let chooseCreature = prompt("Choose a creature")
+// let chooseHobby = prompt("Choose a hobby")
+//
+//
+// function pickNumber(number){
+//     switch (pickNumber){
+//         case isNaN(number):
+//             return "Invalid Number"
+//         case (number == 1):
+//             return "Harry Potter"
+//         case (number == 2):
+//             return "Hermione"
+//         case (number == 3):
+//             return "Granger"
+//         case (number == 4):
+//             return "Ron Weasley"
+//
+//     }
+//
+// }
+// alert(pickNumber(3))
+// let hp=(pickNumber(3)
+// )
+// console.log(hp);
 
 
-function pickNumber(number){
-    switch (pickNumber){
-        case isNaN(number):
-            return "Invalid Number"
-        case (number == 1):
-            return "Harry Potter"
-        case (number == 2):
-            return "Hermione"
-        case (number == 3):
-            return "Granger"
-        case (number == 4):
-            return "Ron Weasley"
+// for (i = 20; i <= 100; i++) {
+//     // body
+// }
 
+// let chooseNumber = prompt("Pick a number 1-4");
+// let chooseColor = prompt('Choose a color');
+// let chooseObject = prompt('Choose an object');
+// let chooseCreature = prompt("Choose a creature");
+// let chooseHobby = prompt("Choose a hobby");
+//
+// function pickNumber() {
+//     switch (Number(number)) {
+//         case 1:
+//             return "Harry Potter";
+//         case 2:
+//             return "Hermione";
+//         case 3:
+//             return "Granger";
+//         case 4:
+//             return "Ron Weasley";
+//         default:
+//             return "Invalid Number";
+//     }
+// }
+//
+// let hp = pickNumber(3);
+// alert(hp);
+// console.log(hp);
+//
+// for (let i = 100; i >= 5; i-=5){
+//     console.log(i)
+// }
+
+
+
+// let chooseNumber = prompt("Pick a number 1-4");
+// let chooseColor = prompt('Choose a color');
+// let chooseObject = prompt('Choose an object');
+// let chooseCreature = prompt("Choose a creature");
+// let chooseHobby = prompt("Choose a hobby");
+
+// function pickNumber(number) {
+//     switch (Number(number)) {
+//         case 1:
+//             return "Harry Potter";
+//         case 2:
+//             return "Hermione";
+//         case 3:
+//             return "Granger";
+//         case 4:
+//             return "Ron Weasley";
+//         default:
+//             return "Invalid Number";
+//     }
+// }
+//
+// let chosenCharacter = pickNumber(chooseNumber);
+// alert(`You picked ${chosenCharacter}.`);
+// console.log(chosenCharacter);
+for (let i = 0; i<10; i++){
+    console.log("this is i: " + i)
+    for (let j=0; j<10; j++){
+        console.log("this is j: " + j)
     }
-
 }
-alert(pickNumber(3))
-let hp=(pickNumber(3)
-)
-console.log(hp);
-
-
--- js/while.js
+++ js/while.js
@@ -1,10 +1,37 @@
 "use strict"
 
+// let num = 2;
+// let output = "";
+//
+// while (num <= 65536) {
+//     output += `${num} `;
+//     num *= 2
+//     console.log(num)
+// }
+
 let num = 2;
 let output = "";
 
 while (num <= 65536) {
-    output += `${num} `;
-    num *= 2
-    console.log(num)
+    output += `${num}\n`;
+    num *= 2;
 }
+
+console.log(output);
+
+
+let allCones = Math.floor(Math.random() * 51) + 50; // Random number between 50 and 100
+
+do {
+    let conesBought = Math.floor(Math.random() * 5) + 1; // Random number between 1 and 5
+
+    if (conesBought <= allCones) {
+        console.log(conesBought + " cones sold...");
+        allCones -= conesBought;
+    } else {
+        console.log("Cannot sell you " + conesBought + " cones. I only have " + allCones + "...");
+    }
+} while (allCones > 0);
+
+console.log("Yay! I sold them all!");
+
